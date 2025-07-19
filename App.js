17d7643291137361dd6cb617fa51120f341d9b44
Mithun Chide <mithunchide03@gmail.com>
2025-07-20 00:39:58 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// const heading = document.createElement('h2')
 
-const heading = document.createElement('h2')
+// heading.innerHTML = 'Hello World using Javascript'
 
-heading.innerHTML = 'Hello World using Javascript'
+// const divElement = document.getElementById('root')
 
-const divElement = document.getElementById('root')
+// divElement.appendChild(heading)
 
-divElement.appendChild(heading)
+
+   const heading = React.createElement("div", {id:'parent'}, [
+        React.createElement('div', {id: 'child'}, [
+        React.createElement('h1', {id:'heading1'}, "Hello Heading 1"),
+        React.createElement('h2', {id:'heading2'}, "Hello Heading 2"),
+        React.createElement('h2', {id:'heading2'}, "Hello Heading 2")
+
+        ]),
+        React.createElement('div', {id: 'child2'}, [
+        React.createElement('h1', {id:'heading1'}, "Hello Heading 1"),
+        React.createElement('h2', {id:'heading2'}, "Hello Heading 2")
+        ])
+    ])
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    root.render(heading)
+
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Namaste react by mithun 
+#Namaste react by mithun
+
+#Parcel
+- Dev Build
+- Local Server
+- HMR (HOt Model refreshing)
+- File Watching Algorithm - written in C++ 
+- Caching - faster builds
+- Image Optimization
+- Minification of files
+- Bundling
+- Compress to the file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support older browser
+- Diagnostics
+- Error Handling
+- It can host on HTTPS server
+- Tree Shaking- It will remove the unused code from your file
+- Different dev and prod bundles

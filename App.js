6bde085f4212859b2269d1243a2d02daca7b8963
Mithun Chide <mithunchide03@gmail.com>
2025-07-19 22:34:47 +0530

    episode 1

--- App.js
+++ App.js
@@ -1,29 +1,9 @@
-// const heading = React.createElement
-// ("h1", 
-// { id : "color"},
-// "Hello World from React");
 
-// creating nested structure as below using react
 
-// <div id="parent">
-/* <div id="child">
-    <h1>I am H1 Tag</h1>
-    </div></div> */
+const heading = document.createElement('h2')
 
-    // if we have more than one sibling then we should have wrap it inside array[]
-    // <div id="parent">
-/* <div id="child">
-    <h1>I am H1 Tag</h1>
-    <h2>I am H2 Tag</h2>
-    </div></div> */
-import React from "react";
-import ReactDOM from "react-dom/client";
+heading.innerHTML = 'Hello World using Javascript'
 
-    const nesteing = React.createElement("div", {id:"parent"}, 
-React.createElement("div", {id:"child"}, [    
-React.createElement("h1", {}, "I am Mithun Chide Learning React Js"),
-React.createElement("h2", {}, "I'm so proud of myself")
-]));
+const divElement = document.getElementById('root')
 
- const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(nesteing);
+divElement.appendChild(heading)
